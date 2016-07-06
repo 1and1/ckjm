bd07754cc424f67e8450c5a164c49e63aaf87ef2
Stephan Fuhrmann <stephan.fuhrmann@1und1.de>
2016-07-06 12:56:47 +0200

    Unused imports

--- src/test/java/org/oneandone/ckjm/test/MetricsFilterTest.java
+++ src/test/java/org/oneandone/ckjm/test/MetricsFilterTest.java
@@ -11,8 +11,6 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Stream;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
